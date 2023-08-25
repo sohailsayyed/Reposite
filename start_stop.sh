f7cde8b1fede45c2ddf75507edfa9e559da664d2
sohailsayyed <72851736+sohailsayyed@users.noreply.github.com>
2023-08-25 15:09:28 +0530

    Create start_stop.sh

---
+++ start_stop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+INSTANCE_ID="i-0d15b392f27070ba4"  # Replace with your instance ID
+
+# Check instance status
+INSTANCE_STATUS=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query "Reservations[0].Instances[0].State.Name" --output text)
+
+if [ "$INSTANCE_STATUS" == "running" ]; then
+  # Instance is already running
+  echo "Instance is already running. Running command..."
+ # ssh -i path/to/key.pem ec2-user@public-ip "touch test.txt"
+else
+  # Instance is not running
+  echo "Starting instance..."
+  aws ec2 start-instances --instance-ids "$INSTANCE_ID"
+
+  # Wait for status checks to pass
+  aws ec2 wait instance-status-ok --instance-ids "$INSTANCE_ID"
+
+  echo "Instance started. Instance 2/2 checks passed.."
+  #ssh -i path/to/key.pem ec2-user@public-ip "touch hello.txt"
+fi
